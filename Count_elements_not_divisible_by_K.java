37fb8e2ce3fc1888dd932e936c73070d28febec0
Sirajh ahmed nazeer <97436615+sirajhahmadnazeer123@users.noreply.github.com>
2022-09-17 13:46:41 +0530

    import java.util.Scanner; class count {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int a,b,i,c=0;         a=sc.nextInt();         b=sc.nextInt();         int [] arr=new int[a];         for (i=0;i<a;i++)         {             arr[i]=sc.nextInt();         }         for(i=0;i<a;i++)         {             if(arr[i]%b!=0)             {                 c+=1;             }         }         System.out.println(c);
        }
    }

---
+++ Count_elements_not_divisible_by_K.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+class count
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a,b,i,c=0;
+        a=sc.nextInt();
+        b=sc.nextInt();
+        int [] arr=new int[a];
+        for (i=0;i<a;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        for(i=0;i<a;i++)
+        {
+            if(arr[i]%b!=0)
+            {
+                c+=1;
+            }
+        }
+        System.out.println(c);
+        
+    }
+}

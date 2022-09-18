e839ebe800a6bc972a432d41b53970f76b9ae5ac
Sirajh ahmed nazeer <97436615+sirajhahmadnazeer123@users.noreply.github.com>
2022-09-18 16:46:34 +0530

    import java.util.Scanner; class average {     public static void main(String args[])     {         Scanner sc = new Scanner(System.in);         int a,i,x[],sum=0;         int h;         a=sc.nextInt();         x = new int[a];         for(i=0;i<a;i++)         {             x[i]=sc.nextInt();         }         for(int ele:x)         {             sum+=ele;         }         h=sum/a;         int n=0;         for(i=0;i<a;i++)         {             if(x[i]<=h)             {                 n++;             }         }         System.out.println(n);     } }

---
+++ elements_smaller_than_average.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+class average
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int a,i,x[],sum=0;
+        int h;
+        a=sc.nextInt();
+        x = new int[a];
+        for(i=0;i<a;i++)
+        {
+            x[i]=sc.nextInt();
+        }
+        for(int ele:x)
+        {
+            sum+=ele;
+        }
+        h=sum/a;
+        int n=0;
+        for(i=0;i<a;i++)
+        {
+            if(x[i]<=h)
+            {
+                n++;
+            }
+        }
+        System.out.println(n);
+    }
+}

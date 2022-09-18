e517f8500106e3d46dc51f8535d844dfb42c431b
Sirajh ahmed nazeer <97436615+sirajhahmadnazeer123@users.noreply.github.com>
2022-09-18 16:38:18 +0530

    import java.util.Scanner; class Solution {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n;         n=sc.nextInt();         int[] a=new int[n];         for(int i=0;i<n;i++)         {             a[i]=sc.nextInt();         }         int c=0;         for(int i=n-1;i>0;i--)         {             if(a[i]%2!=0)             {                 System.out.println(i);                 c++;                 break;             }         }         if(c==0)         {             System.out.println(0);         }     } }

---
+++ last_odd_number_index_position.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+class Solution
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n;
+        n=sc.nextInt();
+        int[] a=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        int c=0;
+        for(int i=n-1;i>0;i--)
+        {
+            if(a[i]%2!=0)
+            {
+                System.out.println(i);
+                c++;
+                break;
+            }
+        }
+        if(c==0)
+        {
+            System.out.println(0);
+        }
+    }
+}

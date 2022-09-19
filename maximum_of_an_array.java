796fbbfbff65f369662041eb10d66fd7e5c5bfb6
Sirajh ahmed nazeer <97436615+sirajhahmadnazeer123@users.noreply.github.com>
2022-09-19 22:11:20 +0530

    import java.util.*; class Solution {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n;         n=sc.nextInt();         int[] a=new int[n];         for(int i=0;i<n;i++)         {             a[i]=sc.nextInt();         }         List<Integer> l=new ArrayList<>();         for(int i=0;i<n;i++)         {             l.add(a[i]);         }         System.out.println(Collections.max(l));     } }

---
+++ maximum_of_an_array.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Solution
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n;
+        n=sc.nextInt();
+        int[] a=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        List<Integer> l=new ArrayList<>();
+        for(int i=0;i<n;i++)
+        {
+            l.add(a[i]);
+        }
+        System.out.println(Collections.max(l));
+    }
+}

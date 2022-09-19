10fa1fecdd83694da8f50fce85ee1f0408684cba
Sirajh ahmed nazeer <97436615+sirajhahmadnazeer123@users.noreply.github.com>
2022-09-19 21:54:39 +0530

    import java.util.Scanner; class Sum {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int x[],i,n;         n=sc.nextInt();         x=new int[n];         for(i=0;i<n;i++)         {             x[i]=sc.nextInt();         }         int a=sc.nextInt();         int b=sc.nextInt();         int l=0;         for(i=0;i<n;i++)         {             if(x[i]<a || x[i]>b)             {                 System.out.print(x[i]+" ");                    l++;             }         }         if(l==0)         {            System.out.print("-1");         }     } }

---
+++ elements_not_in_between_A_and_B.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+class Sum
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int x[],i,n;
+        n=sc.nextInt();
+        x=new int[n];
+        for(i=0;i<n;i++)
+        {
+            x[i]=sc.nextInt();
+        }
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int l=0;
+        for(i=0;i<n;i++)
+        {
+            if(x[i]<a || x[i]>b)
+            {
+                System.out.print(x[i]+" ");
+                   l++;
+            }
+        }
+        if(l==0)
+        {
+           System.out.print("-1"); 
+        }
+    }
+}

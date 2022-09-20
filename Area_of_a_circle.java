0fe58d8311daedb5f61c12c4a32f73743b48b5a6
Sirajh ahmed nazeer <97436615+sirajhahmadnazeer123@users.noreply.github.com>
2022-09-20 20:38:45 +0530

    import java.util.*; class circle {     public static void main(String args[])     {         Scanner sc = new Scanner(System.in);         int a;         double pi=3.14;         float b;         a=sc.nextInt();         b=(float)pi*a*a;         System.out.format("%.2f",b);     } }

--- Area_of_a_circle.java
+++ Area_of_a_circle.java
@@ -1,13 +1,14 @@
-import java.util.Scanner;
-class deepak
+import java.util.*;
+class circle
 {
     public static void main(String args[])
     {
-        Scanner sc= new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         int a;
-        float c;
+        double pi=3.14;
+        float b;
         a=sc.nextInt();
-        c=(float)((a*a)*3.14);
-        System.out.format("%.2f",c);
+        b=(float)pi*a*a;
+        System.out.format("%.2f",b);
     }
 }

4547c1a37eb993c52a28f5c84baccac858482e54
Sirajh ahmed nazeer <97436615+sirajhahmadnazeer123@users.noreply.github.com>
2022-09-20 20:42:25 +0530

    import java.util.*; class simple {     public static void main(String args[])     {         Scanner sc =new Scanner (System.in);         int p,t,r;         int l;         p=sc.nextInt();         t=sc.nextInt();         r=sc.nextInt();         l=p*t*r/100;         System.out.println(l);     } }

--- SIMPLE_INTEREST.java
+++ SIMPLE_INTEREST.java
@@ -1,16 +1,15 @@
-import java.util.Scanner;
-class sio
-           {
-                        public static void main(String args[ ])
-                             {
-                                       Scanner sc=new Scanner(System.in);
-                                         int a,b,c,d;
-                                         a=sc.nextInt();
-                                         b=sc.nextInt();
-                                         c=sc.nextInt();
-                                          d=(a*b*c)/100;
-                                         System.out.println(d);
-                                
-                                }
-                 }    
-                                       
+import java.util.*;
+class simple
+{
+    public static void main(String args[])
+    {
+        Scanner sc =new Scanner (System.in);
+        int p,t,r;
+        int l;
+        p=sc.nextInt();
+        t=sc.nextInt();
+        r=sc.nextInt();
+        l=p*t*r/100;
+        System.out.println(l);
+    }
+}

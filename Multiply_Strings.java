af09fc48a1bf83e46bb680d679bf5cc44c0b251d
Sirajh ahmed nazeer <97436615+sirajhahmadnazeer123@users.noreply.github.com>
2022-09-23 22:35:09 +0530

    import java.util.Scanner; class multiple {     public static void main(String args[])     {         Scanner sc = new Scanner(System.in);         long s,p;         s=sc.nextLong();         p=sc.nextLong();         System.out.println(p*s);     } }

---
+++ Multiply_Strings.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class multiple
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        long s,p;
+        s=sc.nextLong();
+        p=sc.nextLong();
+        System.out.println(p*s);
+    }
+}

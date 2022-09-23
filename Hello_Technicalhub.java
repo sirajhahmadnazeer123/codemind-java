01e1d82b95de11711efe90ba4adc8980105ad928
Sirajh ahmed nazeer <97436615+sirajhahmadnazeer123@users.noreply.github.com>
2022-09-23 22:28:33 +0530

    import java.util.Scanner; class thub {     public static void main(String args[])     {             Scanner sc = new Scanner(System.in);             String s;             s=sc.nextLine();             System.out.println("Hello Technicalhub");             System.out.println(s);
        }
    }

---
+++ Hello_Technicalhub.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class thub
+{
+    public static void main(String args[])
+    {
+            Scanner sc = new Scanner(System.in);
+            String s;
+            s=sc.nextLine();
+            System.out.println("Hello Technicalhub");
+            System.out.println(s);
+            
+    }
+}

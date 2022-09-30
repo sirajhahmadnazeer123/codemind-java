8dfb706526f83030a02b9b20679ad905d3e94d6c
Sirajh ahmed nazeer <97436615+sirajhahmadnazeer123@users.noreply.github.com>
2022-09-30 15:30:08 +0530

    import java.util.Scanner; class Plow {     public static void main(String args[])     {         Scanner sc = new Scanner(System.in);         String d=sc.nextLine();         System.out.println(d.toLowerCase());     } }

---
+++ To_Lower_Case.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class Plow
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        String d=sc.nextLine();
+        System.out.println(d.toLowerCase());
+    }
+}

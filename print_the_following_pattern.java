dc95c3ada2836f1c737682423bc976000eb13064
Sirajh ahmed nazeer <97436615+sirajhahmadnazeer123@users.noreply.github.com>
2022-10-27 10:42:42 +0530

    import java.util.Scanner; class Pattern {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int a=sc.nextInt();         int i,j;         for(i=1;i<=a;i++)         {             for(j=1;j<=a;j++)             {                 if(i==j || j==1 || (i+j)==(a+j))                 {                     System.out.print("*");                 }                 else                 {                     System.out.print(" ");                 }             }             System.out.println();         }     } }

--- print_the_following_pattern.java
+++ print_the_following_pattern.java
@@ -1,21 +1,25 @@
 import java.util.Scanner;
- 
- class MainClass
+class Pattern
 {
-    public static void main(String[] args) 
+    public static void main(String args[])
     {
-        Scanner sc = new Scanner(System.in);
-        int rows = sc.nextInt();
-         for (int i = rows; i >= 1; i--)
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int i,j;
+        for(i=1;i<=a;i++)
         {
-            for (int j = 1; j <= i; j++)
+            for(j=1;j<=a;j++)
             {
-                System.out.print(j);
+                if(i==j || j==1 || (i+j)==(a+j))
+                {
+                    System.out.print("*");
+                }
+                else
+                {
+                    System.out.print(" ");
+                }
             }
-             
             System.out.println();
         }
-         
-         
     }
 }
